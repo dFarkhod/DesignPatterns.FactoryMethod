1e240c9aec91a54f12a359d46ee2c6f2551beeb6
Farkhod <dfarkhod@gmail.com>
2022-08-20 19:33:57 +0800

    added solution to handle open close principle

---
+++ CoffeeFactoryContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class CoffeeFactoryContainer
+    {
+        private Dictionary<string, ICoffeeFactory> factories = new Dictionary<string, ICoffeeFactory>();
+
+        public void Register(string name, ICoffeeFactory factory)
+        {
+            factories.Add(name, factory);
+        }
+
+        public ICoffeeFactory GetFactory(string name)
+        {
+           return factories.ContainsKey(name) ? factories[name] :  null;
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -5,11 +5,22 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        // 1-misol, to'g'ridan to'g'ri kerakli factory'ni ishlatish:
         ICoffeeFactory cappucinoFactory = new CappucinoFactory();
         var myCappucino = cappucinoFactory.Create();
 
         ICoffeeFactory mochaFactory = new MochaFactory();
         var myMocha = mochaFactory.Create();
 
+
+
+        // 2-misol, factory'larni container'da ro'yxatga olib ishlatish:
+        var factoryContainer = new CoffeeFactoryContainer();
+
+        factoryContainer.Register("cappucino", new CappucinoFactory());
+        factoryContainer.Register("mocha", new MochaFactory());
+        factoryContainer.Register("americano", new AmericanoFactory());
+
+        var myCappucino2 = factoryContainer.GetFactory("cappucino")?.Create();
     }
 }
